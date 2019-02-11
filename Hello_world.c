f3521e296bda5cb1e4d8f132b6ad453168653dfc
samael98 <33547775+samael98@users.noreply.github.com>
2019-02-11 13:34:36 +0530

    Create Hello_world.c

---
+++ Hello_world.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+printf("Hey people! My name is Satvik Mishra and this is my first program on GitHub./n Series of other commands and projects will be live soon. /n Stay tuned.");
+/*this is a comment and the above printf commnad is a printing command in C language*/
+return 0;
+}
